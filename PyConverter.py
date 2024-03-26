f7bfb66730c3c7260a090b1c2c2c2e14364f7cd5
Kona Fatsea <fatseakona@gmail.com>
2024-03-26 20:18:51 +0100

    Working on Converter - update 1

--- PyConverter.py
+++ PyConverter.py
@@ -2,11 +2,10 @@ import PySimpleGUI as sg
 
 # STEP 1 define the layout 
 layout = [
-    [sg.Text('Gender', enable_events=True, key='-TEXT-'),sg.Spin(['male','female'], key='-GENDER-')],
-    [sg.Button('Button', key = '-BUTTON1-')],
-    [sg.Input(key='-NAME-')],
-    [sg.Text('Hello'), sg.Button('Print', key = '-BUTTON2-')]
+    [sg.Input(s = 15, key='-INPUT-'), sg.Button('Convert', key='-BUTTON1-')],
+    [sg.Text('Result', key='-RESULT-')]
 ]
+
 
 # STEP 2 - create the window
 window = sg.Window('Converter', layout)
@@ -14,18 +13,12 @@ window = sg.Window('Converter', layout)
 # STEP 3 - the event loop
 while True:
     event, values = window.read() # Read the event that happened and the values dictionary
-    #print(event, values) 
+    print(event, values) 
 
     if event == sg.WIN_CLOSED:
         break
 
     if event == '-BUTTON1-':
-        window['-TEXT-'].update(visible = False)
-
-    if event == '-BUTTON2-':
-        print(values['-NAME-'])
-    
-    if event == '-TEXT-': # Treat text as button
-        print('text was pressed')
+        window['-RESULT-'].update(values['-INPUT-'])
 
 window.close()

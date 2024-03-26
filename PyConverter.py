ead1e32b28812705c9cc5ae1a23d0e5210ade3c8
Kona Fatsea <fatseakona@gmail.com>
2024-03-26 21:48:11 +0100

    Python Converter Completed

--- PyConverter.py
+++ PyConverter.py
@@ -2,15 +2,18 @@ import PySimpleGUI as sg
 
 # STEP 1 define the layout 
 layout = [
-    [sg.Input(s = 15, key='-INPUT-'), sg.Button('Convert', key='-BUTTON1-')],
-    [sg.Text('Result', key='-RESULT-')]
+    [
+        sg.Input(key = '-INPUT-'),
+        sg.Spin(['km to mile', 'kg to pound', 'sec to min'], key = '-UNITS-'),
+        sg.Button('Convert', key = '-CONVERT-')
+    ],
+    [sg.Text('Output', key = '-OUTPUT-')]
 ]
 
-
-# STEP 2 - create the window
+# STEP 2 - Create the window
 window = sg.Window('Converter', layout)
 
-# STEP 3 - the event loop
+# STEP 3 - The event loop
 while True:
     event, values = window.read() # Read the event that happened and the values dictionary
     print(event, values) 
@@ -18,7 +21,24 @@ while True:
     if event == sg.WIN_CLOSED:
         break
 
-    if event == '-BUTTON1-':
-        window['-RESULT-'].update(values['-INPUT-'])
+    if event == '-CONVERT-':
+
+        input_value = values['-INPUT-']
+
+        if input_value.isnumeric():
+            match values['-UNITS-']:
+                case 'km to mile':
+                    output = float(input_value) * 0.6214
+                    output_string = f'{input_value} km are {output} miles.'
+                case 'kg to pound':
+                    output = float(input_value) * 2.205
+                    output_string = f'{input_value} kg are {output} pounds.'
+                case 'sec to min':
+                    output = float(input_value) / 60
+                    output_string = f'{input_value} sec are {output} minutes.'
+            window['-OUTPUT-'].update(output_string)
+
+        else: 
+            window['-OUTPUT-'].update('Please enter a number')
 
 window.close()
--- temp.py
+++ temp.py
@@ -1,5 +1,4 @@
 
-
 import PySimpleGUI as sg
 
 # Define the layout

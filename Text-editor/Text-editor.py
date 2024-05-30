80c67052c0cb9bf06bda58bb86eecda2c5204273
Kona Fatsea <fatseakona@gmail.com>
2024-05-30 17:50:16 +0200

    fix: Resolve bug causing inaccurate word count due to multiple line breaks in text input

--- Text-editor/Text-editor.py
+++ Text-editor/Text-editor.py
@@ -7,7 +7,7 @@ smileys = [
     'other', [':3']
 ]
 
-smiley_events = smileys[1] + smileys[3] + smileys[5]
+smiley_events = smileys[1] + smileys[3] + smileys[5] #to make events accessilble
 
 menu_layout = [ 
     ['File', ['Open', 'Save', '---', 'Exit']],
@@ -19,7 +19,7 @@ menu_layout = [
 layout = [
     [sg.Menu(menu_layout, k = '-MENU-')],
     [sg.Text("Unsaved"), sg.Button('Popup Button')],
-    [sg.Multiline(size = (40,30), key = '-TEXTBOX-', no_scrollbar = True)]
+    [sg.Multiline(size = (40,20), key = '-TEXTBOX-', no_scrollbar = True)]
 ]
 
 
@@ -37,12 +37,14 @@ while True:
     if event == 'Word Count':
         full_text = values['-TEXTBOX-']
         clean_text = full_text.replace('\n',' ').split(' ')
+        clean_text = [word for word in clean_text if word != '']
         word_count = len(clean_text)
+        print(f'full text:{full_text}\nclean text:{clean_text}\nword count:{word_count}')
         char_count = len(''.join(clean_text))
         sg.Popup(f'words:{word_count}\ncharacters: {char_count}')
 
     if event == 'Open':
-        file_path = sg.popup_get_file("Choose a file: ", no_window = True)
+        file_path = sg.popup_get_file("Text not shown", no_window = True)
         if file_path:
             print("File Selected", file_path)
             with open(file_path, 'r') as file:
@@ -56,6 +58,9 @@ while True:
         emoji = event 
         updated_text = full_text + ' ' + emoji
         window['-TEXTBOX-'].update(updated_text)
+    
+    # if event == 'Save':
+
       
 
 

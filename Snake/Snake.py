843c1b869c61a80d168019001e5005b9be2ef32c
Kona Fatsea <fatseakona@gmail.com>
2024-06-26 13:22:11 +0200

    SNAKE - New Element - Graph
    - Introduced a new Graph element for the game field
    - Set the canvas size and background color to black
    - Added the graph element to the window layout

--- Snake/Snake.py
+++ Snake/Snake.py
@@ -1,9 +1,18 @@
 import PySimpleGUI as sg 
 
-# Define layout
-layout = [
+# game constants
+FIELD_SIZE = 400
 
-]
+# Define layout
+sg.theme('Green')
+field = sg.Graph(
+   canvas_size = (FIELD_SIZE, FIELD_SIZE),
+    graph_bottom_left = (0,0),
+    graph_top_right = (FIELD_SIZE, FIELD_SIZE),
+    background_color = 'black'
+)
+
+layout = [[field]]
 
 
 # Create window
